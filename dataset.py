39613e41c58d814c112e81cbcc11eb92b26f19d9
icanswim <wardsj@hotmail.com>
2021-03-10 10:51:42 -0800

    updates

--- dataset.py
+++ dataset.py
@@ -9,31 +9,34 @@ import pandas as pd
 from cosmosis.dataset import CDataset
 
 
+
 class Ranzcr(CDataset):
     """https://www.kaggle.com/c/ranzcr-clip-catheter-line-classification/overview
     """
     def load_data(self, image_dir='./data/ranzcr/train/', 
-                        targets_csv='./data/ranzcr/train.csv',
+                        target_csv='./data/ranzcr/train.csv',
                         target_type=None):
         """creates datadic['pt_id']=(cxr_image_filename,target)
-        target_type = 'ETT','NG','CVC','SWAN'
+        target_type = None,'ETT','NG','CATH','SWAN','CVC'
         """
-        df = pd.read_csv(targets_csv, header=0, index_col=0)
+        df = pd.read_csv(target_csv, header=0, index_col=0)
         
         data = {}
         for file in os.listdir(image_dir):
             targets = df.loc[[file[:-4]]].to_numpy()
-            target = np.reshape(targets, -1)[:-1].astype(np.float64)
+            target = np.reshape(targets, -1)[:-1].astype(np.int64)
 
             if target_type == 'ETT':
                 target = np.reshape(target[:3], -1)
             if target_type == 'NG':
-                target = np.reshape(target[4:7], -1)
-            if target_type == 'CVC':
-                target = np.reshape(target[8:10], -1)
+                target = np.reshape(target[3:7], -1)
+            if target_type == 'CATH':
+                target = np.reshape(target[7:11], -1)
             if target_type == 'SWAN':
-                target = np.reshape(target[11], -1)
+                target = np.reshape(target[10], -1)
+            if target_type == 'CVC':
+                target = np.reshape(target[7:10], -1)
 
-            data[file[26:-4]] = (image_dir+'/'+file, target)
+            data[file[:-4]] = (image_dir+'/'+file, target)
    
         return data
